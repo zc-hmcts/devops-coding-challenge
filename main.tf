06dfe9baaf68c4f92b08259039edaca493f8c5c8
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2022-10-31 22:39:11 +0000

    rm

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.3.0"
-  backend "azurerm" {
-    storage_account_name = "devopstestsa"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth     = true
-    subscription_id      = "a4ccfc3f-2800-474b-b2de-bdd021a510c1"
-    tenant_id            = "18469708-093e-42c9-9dbf-7f57ac8f8d0e"
-  }
+  backend "azurerm" {}
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -18,7 +11,7 @@ terraform {
 data "terraform_remote_state" "foo" {
   backend = "azurerm"
   config = {
-    storage_account_name = "devopstestsa"
+    storage_account_name = "devopstestsatf"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
